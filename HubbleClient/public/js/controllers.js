f3ded8f4d029bca9349de569db9960e56b97d891
Harshit Mehrotra <hmehrotra.86@gmail.com>
2014-03-30 21:37:54 -0700

    Entity list view setup with dummy data

--- HubbleClient/public/js/controllers.js
+++ HubbleClient/public/js/controllers.js
@@ -5,10 +5,15 @@ define(function() {
     /* Controllers */
     var controllers = {};
 
-    controllers.LoginController = function() {
-
+    controllers.LoginController = function($scope, $http) {
+        $http.get('sampleData/EcosystemSegments.json').success(function(data) {
+            $scope.segments = data.segmentNames;
+            $scope.companies = data.segmentNames;
+            $scope.countries = data.segmentNames;
+        });
     };
-    controllers.LoginController.$inject = [];
+    controllers.LoginController.$inject = ['$scope', '$http'];
 
     return controllers;
 });
+
