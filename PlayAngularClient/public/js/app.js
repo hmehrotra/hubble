e713956102be3858d4be2c777523a5cc3e77608f
Harshit Mehrotra <hmehrotra.86@gmail.com>
2015-01-17 01:34:00 -0800

    Integrate resolve property for entity list view loading

--- PlayAngularClient/public/js/app.js
+++ PlayAngularClient/public/js/app.js
@@ -1,30 +1,10 @@
 /* Run in strict mode to avoid debugging nightmares */
 'use strict';
 
-/* require(['angular', './controllers', './directives', './filters', './services', 'angular-route'],
-    function(angular, controllers) {
-
-        // Declare app level module which depends on filters, and services
-
-        /* Setter for module dependencies
-        angular.module('hubble', ['ngRoute']).
-            config(['$routeProvider' , function($routeProvider){
-            $routeProvider.when('/login', { templateUrl: 'views/loginPage.html', controller: controllers.LoginController });
-            $routeProvider.when('/home', { templateUrl: 'views/homePage.html', controller: controllers.HubbleController });
-            $routeProvider.otherwise({redirectTo: '/home'});
-        }]);
-
-        angular.bootstrap(document, ['hubble']);
-    }); */
-
-// Getter for module dependencies
-// angular.module("hubble");
-
 require(['angular',
          './libs/angular-ui-router',
-         './segmentList/SegmentListController',
          'angular-route'],
-    function(angular, undefined, segmentListCtrl){
+    function(angular){
         angular.module('hubble',['ui.router', 'ngRoute'])
            .config(['$routeProvider', '$stateProvider', function($routeProvider, $stateProvider){
 
@@ -43,38 +23,41 @@ require(['angular',
                         views: {
                             'segmentListView' : {
                                 templateUrl: 'js/segmentList/SegmentListView.html',
-                                controller: function($scope, $http){
-                                    $http.get('../sampleData/EcosystemSegments.json')
-                                            .success(function(data) {
-                                                $scope.segments = data.segmentNames;
-                                            })
-                                            .error(function(data, status, headers, config){
-                                                console.log('Error occurred' + status);
-                                            });
+                                controller: function($scope, segments){
+                                    $scope.segments = segments;
+                                },
+                                resolve: {
+                                    segments: ['$http', function($http){
+                                        return $http.get('../sampleData/EcosystemSegments.json').then(function(response){
+                                            return response.data.segmentNames;
+                                        })
+                                    }]
                                 }
                             },
                             'companyListView' : {
                                 templateUrl: 'js/companyList/CompanyListView.html',
-                                controller: function($scope, $http){
-                                    $http.get('../sampleData/Companies.json')
-                                          .success(function(data) {
-                                             $scope.companies = data.companies;
+                                controller: function($scope, companies){
+                                      $scope.companies = companies;
+                                },
+                                resolve: {
+                                      companies: ['$http', function($http){
+                                          return $http.get('../sampleData/Companies.json').then(function(response){
+                                              return response.data.companies;
                                           })
-                                          .error(function(data, status, headers, config){
-                                             console.log('Error occurred' + status);
-                                          });
+                                      }]
                                 }
                             },
                             'countryListView' : {
                                 templateUrl: 'js/countryList/CountryListView.html',
-                                controller: function($scope, $http){
-                                    $http.get('../sampleData/Countries.json')
-                                         .success(function(data) {
-                                             $scope.countries = data.countries;
-                                         })
-                                         .error(function(data, status, headers, config){
-                                             console.log('Error occurred' + status);
-                                         });
+                                controller: function($scope, countries){
+                                       $scope.countries = countries;
+                                },
+                                resolve: {
+                                       countries: ['$http', function($http){
+                                            return $http.get('../sampleData/Countries.json').then(function(response){
+                                                return response.data.countries;
+                                            })
+                                       }]
                                 }
                             }
                         }
